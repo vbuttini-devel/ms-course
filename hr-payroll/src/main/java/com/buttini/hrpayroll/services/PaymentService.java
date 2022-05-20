61cb0e5fa5a0b027ed02cdbfb80336e3018f9edd
vbuttini-devel <vinicius.buttini@develcode.com.br>
2022-05-20 13:56:22 -0300

    update

--- hr-payroll/src/main/java/com/buttini/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/buttini/hrpayroll/services/PaymentService.java
@@ -4,12 +4,7 @@ import com.buttini.hrpayroll.entities.Payment;
 import com.buttini.hrpayroll.entities.Worker;
 import com.buttini.hrpayroll.feignclientes.WorkerFeignClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
--- hr-worker/src/main/java/com/buttini/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/buttini/hrworker/resources/WorkerResource.java
@@ -3,6 +3,9 @@ package com.buttini.hrworker.resources;
 import com.buttini.hrworker.entities.Worker;
 import com.buttini.hrworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,10 @@ import java.util.List;
 @RequestMapping("/worker")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    private final Environment env;
+
     private final WorkerRepository workerRepository;
 
     @GetMapping
@@ -27,6 +34,7 @@ public class WorkerResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findId(@PathVariable Long id){
+        logger.info("PORT= " + env.getProperty("local.server.port"));
         return ResponseEntity.ok().body(
                 workerRepository.findById(id)
                         .orElseThrow(()-> new RuntimeException("Worker not found"))
